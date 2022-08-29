e640194a41ba5e48f463c178918a02fff4719f00
piginzoo <piginzoo@gmail.com>
2022-08-29 21:51:26 +0800

    bugfix

--- mlstock/ml/backtest.py
+++ mlstock/ml/backtest.py
@@ -72,10 +72,13 @@ def select_stocks_by_pred(df):
     df_pct = DataFrame()
     df_selected_stocks = DataFrame()
     for date, df_group in df_groups:
+        import pdb;pdb.set_trace()
         df_top30 = df_group.iloc[0:30, :]
+
         next_pct_chg_mean = np.mean(df_top30.next_pct_chg.values)
         next_pct_chg_baseline_mean = np.mean(df_top30.next_pct_chg_baseline.values)
         df_pct = df_pct.append([[date, next_pct_chg_mean, next_pct_chg_baseline_mean]])
+
         df_top30['trade_date'] = date
         df_selected_stocks = df_selected_stocks.append(df_top30)
 
