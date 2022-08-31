3d635691bb5473a57107911c0e0b9870effe27c1
piginzoo <piginzoo@gmail.com>
2022-08-31 22:27:22 +0800

    bugfx

--- mlstock/factors/mixin/fill_mixin.py
+++ mlstock/factors/mixin/fill_mixin.py
@@ -27,17 +27,17 @@ class FillMixin:
         if type(finance_column_names)!=list:
             finance_column_names = [finance_column_names]
 
-        # 按照公布日期倒序排（日期从新到旧）,<=== '倒'序很重要，这样才好找到对应日的财务数据
-        df_finance = df_finance.sort_values('ann_date')
         # 开始做join合并，注意注意，用outer，外连接，这样就不会落任何两边的日期（财务的，和，股票交易数据的）
         df_merge = df_stocks.merge(df_finance,
                                    how="outer",
                                    left_on=['ts_code','trade_date'],
                                    right_on=['ts_code','ann_date'])
         # import pdb;pdb.set_trace()
-        # 因为做了join合并，所以必须要做一个日期排序，因为日期是乱的
+        # 因为做了join合并，所以必须要做一个日期排序，因为日期是乱的（默认是升序，从旧到新）
         df_merge = df_merge.sort_values(['ts_code','trade_date'])
+
         # 然后，就可以对指定的财务字段，进行填充了，先用ffill，后用bfill
+        # ffill从上向下填充，即用旧日期的去填充新日期的
         df_merge[finance_column_names] = df_merge.groupby('ts_code').ffill().bfill()[finance_column_names]
         # 取close这一列（它在股票数据里肯定不是空，但被outer-join后，财务日期对应的close肯定是空，用它来过滤），来过滤掉无效数据
         df_merge = df_merge[~df_merge['close'].isna()]
