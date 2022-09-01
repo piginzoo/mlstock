01cf5c552eceb222518b8063767e9cd7369ae29a
piginzoo <piginzoo@gmail.com>
2022-09-01 21:42:20 +0800

    bugfix

--- mlstock/research/prepare_train_backtest_for_one_factor.py
+++ mlstock/research/prepare_train_backtest_for_one_factor.py
@@ -8,14 +8,13 @@ from mlstock.utils import utils
 
 def main(factor_name):
     start_date = '20090101'
-    split_date = '20190101'
     end_date = '20220901'
 
     factor_class = factor_conf.get_factor_class_by_name(factor_name)
     # DailyIndicator 始终需要这个因子，因为里面有市值log信息，用于市值中性化
     _, _, csv_path = factor_service.calculate([factor_class,DailyIndicator],
                                               start_date,
-                                              split_date,
+                                              end_date,
                                               num=50,
                                               is_industry_neutral=True)
     train_backtest_for_each_factor.main(csv_path, factor_name)
