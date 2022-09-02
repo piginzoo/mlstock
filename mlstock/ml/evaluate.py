b97af8bac0bf072e5a0d169ee7f2a09007be03b0
piginzoo <piginzoo@gmail.com>
2022-09-02 15:32:43 +0800

    bugfix

--- mlstock/ml/evaluate.py
+++ mlstock/ml/evaluate.py
@@ -73,7 +73,8 @@ def factor_weights(model):
     param_names = factor_conf.get_factor_names()
     df = pd.DataFrame({'names': param_names, 'weights': param_weights})
     df = df.reindex(df.weights.abs().sort_values().index)
-    logger.info("参数和权重排序：\n%r", df.to_string().replace('\n', '\n\t'))
+    logger.info("参数和权重排序：")
+    logger.info(df.to_string().replace('\n', '\n\t'))
 
 
 def main(args):
