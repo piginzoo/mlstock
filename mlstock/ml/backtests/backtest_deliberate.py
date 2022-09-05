47a1c288abef584896ca8c11b20922bdc9084f30
piginzoo <piginzoo@gmail.com>
2022-09-05 15:37:16 +0800

    bugfix

--- mlstock/ml/backtests/backtest_deliberate.py
+++ mlstock/ml/backtests/backtest_deliberate.py
@@ -46,9 +46,8 @@ class Broker:
     def distribute_cash(self):
         if self.cash <= 0:
             return None
-        current_positions = len(self.positions)
-        available_positions = stock_num - current_positions
-        cash4stock = math.floor(self.cash / available_positions)
+        buy_stock_num = self.get_buy_trade_num()
+        cash4stock = math.floor(self.cash / buy_stock_num)
         return cash4stock
 
     def sell(self, trade, trade_date):
@@ -64,11 +63,10 @@ class Broker:
         amount = price * position
         commission = amount * sell_commission_rate
 
-        # 更新头寸
+        # 更新头寸,仓位,交易历史
+        self.trades.pop(trade)
         self.cashin(amount - commission)
-        # 更新仓位
         self.positions.pop(trade.ts_code, None)  # None可以防止pop异常
-        # 保留交易历史
         trade.trade_date = trade_date
         self.trade_history.append(trade)
 
@@ -95,7 +93,8 @@ class Broker:
         # 看看实际能卖多少手
         position = 100 * ((cash4stock / price) // 100)  # 最小单位是1手=100股
         if position == 0:
-            logger.warning("现金[%.2f]已经不够为股票[%s]分配资金了", self.cash, trade.ts_code)
+            logger.warning("资金分配失败：从总现金[%.2f]中分配[%.2f]给股票[%s]（价格%.2f）失败",
+                           self.cash, cash4stock, trade.ts_code, price)
             return False
 
         # 计算实际费用
@@ -103,13 +102,10 @@ class Broker:
         # 计算佣金
         commission = buy_commission_rate * actual_cost
 
-        # 更新仓位
+        # 更新仓位,头寸,交易历史
+        self.trades.pop(trade)
         self.positions[trade.ts_code] = position
-
-        # 更新头寸
         self.cashout(actual_cost + commission)
-
-        # 保留交易历史
         trade.trade_date = trade_date
         self.trade_history.append(trade)
 
@@ -135,11 +131,14 @@ class Broker:
     def clear_buy_trades(self):
         self.trades = [t for t in self.trades if t.action == 'sell']
 
-    def is_in_sell_trades(self,ts_code):
+    def is_in_sell_trades(self, ts_code):
         for t in self.trades:
-            if t.action != 'sell':continue
+            if t.action != 'sell': continue
             if t.ts_code == ts_code: return True
         return False
+
+    def get_buy_trade_num(self):
+        return len([t for t in self.trades if t.action == 'buy'])
 
     def handle_adjust_day(self, day_date):
         """
@@ -158,7 +157,7 @@ class Broker:
                 logger.info("待买股票[%s]已经在仓位中，无需卖出", ts_code)
                 continue
             if self.is_in_sell_trades(ts_code):
-                logger.warning("股票[%s]已经在卖单中，可能是还未卖出，无需再创建卖单了",ts_code)
+                logger.warning("股票[%s]已经在卖单中，可能是还未卖出，无需再创建卖单了", ts_code)
             else:
                 self.trades.append(Trade(ts_code, day_date, 'sell'))
                 logger.debug("%s ，创建卖单，卖出持仓股票 [%s]", day_date, ts_code)
@@ -194,9 +193,9 @@ class Broker:
 
         total_value = total_position_value + self.cash
         self.df_values = self.df_values.append({'trade_date': trade_date,
-                               'total_value': total_value,
-                               'total_position_value': total_position_value,
-                               'cash': self.cash}, ignore_index=True)
+                                                'total_value': total_value,
+                                                'total_position_value': total_position_value,
+                                                'cash': self.cash}, ignore_index=True)
         logger.debug("更新 %s 日的市值 %.2f = %d只股票市值 %.2f + 持有的现金 %.2f",
                      trade_date, total_value, len(self.positions), total_position_value, self.cash)
 
@@ -218,6 +217,12 @@ class Broker:
                     is_transaction_succeeded.append(self.sell(trade, day_date))
                     continue
                 raise ValueError(f"无效的交易类型：{trade.action}")
+
+            buy_trades = [x for x in self.trades if x.action == 'buy']
+            for trade in buy_trades:
+                self.buy(trade)
+
+            sell_trades = [x for x in self.trades if x.action == 'sell']
 
             # 保留那些失败的交易，等待明天重试
             self.trades = [self.trades[i] for i, b in enumerate(is_transaction_succeeded) if not b]
@@ -249,15 +254,17 @@ def main(data_path, start_date, end_date, model_pct_path, model_winloss_path, fa
     df_portfolio.sort_values('trade_date')
 
     # 把基准收益拼接进去
-    df_baseline = df_data[['trade_date','next_pct_chg_baseline']].drop_duplicates()
+    df_baseline = df_data[['trade_date', 'next_pct_chg_baseline']].drop_duplicates()
     # 只筛出来周频的市值来
-    df_portfolio = df_baseline.merge(df_portfolio,how='left',on='trade_date')
+    df_portfolio = df_baseline.merge(df_portfolio, how='left', on='trade_date')
 
     # 准备pct、next_pct_chg、和cumulative_xxxx
     df_portfolio['pct_chg'] = df_portfolio.total_value.pct_change()
     df_portfolio['next_pct_chg'] = df_portfolio.pct_chg.shift(-1)
     df_portfolio[['cumulative_pct_chg', 'cumulative_pct_chg_baseline']] = \
         df_portfolio[['next_pct_chg', 'next_pct_chg_baseline']].apply(lambda x: (x + 1).cumprod() - 1)
+
+    df_portfolio = df_portfolio[~df_portfolio.cumulative_pct_chg.isna()]
 
     plot(df_portfolio, start_date, end_date, factor_names)
 
