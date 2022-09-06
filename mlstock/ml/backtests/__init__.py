d669f5d419d753902ebaf9011c60c56ba0e70513
piginzoo <piginzoo@gmail.com>
2022-09-06 15:34:17 +0800

    bugfix

--- mlstock/ml/backtests/__init__.py
+++ mlstock/ml/backtests/__init__.py
@@ -80,6 +80,15 @@ def predict(data_path, start_date, end_date, model_pct_path, model_winloss_path,
     return df_data
 
 
+# 画A股,参考：https://deepinout.com/matplotlib/matplotlib-axis/matplotlib-the-hidden-scale-mode-shows-the-three-y-axes.html
+# 创建第三条Y轴，把第三条Y轴的其它三边隐藏起来，只留下右边显示
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.values():
+        sp.set_visible(False)
+
+
 def plot(df, start_date, end_date, factor_names):
     """
     1. 每期实际收益
@@ -97,53 +106,54 @@ def plot(df, start_date, end_date, factor_names):
     y2 = df.cumulative_pct_chg.values
     y3 = df.cumulative_pct_chg_baseline.values
     y4 = df.index_close.values
+
     color_y1 = '#2A9CAD'
     color_y2 = "#FAB03D"
     color_y3 = "#FAFF0D"
+    color_y4 = "#00FF00"
+
     title = '资产组合收益率及累积收益率'
+
     label_x = '周'
     label_y1 = '资产组合周收益率'
     label_y2 = '资产组合累积收益率'
     label_y3 = '基准累积收益率'
+    label_y4 = '上证指数'
+
     fig, ax1 = plt.subplots(figsize=(10, 6), dpi=300)
     plt.xticks(rotation=60)
     ax2 = ax1.twinx()  # 做镜像处理
 
     ax1.bar(x=x, height=y1, label=label_y1, color=color_y1, alpha=0.7)
-    ax2.plot(x, y2, color=color_y2, ms=10, label=label_y2)
-    ax2.plot(x, y3, color=color_y3, ms=10, label=label_y3)
-
     ax1.set_xlabel(label_x)  # 设置x轴标题
     ax1.set_ylabel(label_y1)  # 设置Y1轴标题
-    ax2.set_ylabel(label_y2 + "/" + label_y3)  # 设置Y2轴标题
     ax1.grid(False)
-    ax2.grid(False)
-
     # 12周间隔，3个月相当于，为了让X轴稀疏一些，太密了，如果不做的话
     ax1.xaxis.set_major_locator(ticker.MultipleLocator(12))
+
+    ax2.plot(x, y2, color=color_y2, ms=10, label=label_y2)
+    ax2.plot(x, y3, color=color_y3, ms=10, label=label_y3)
+    ax2.set_ylabel(label_y2 + "/" + label_y3)  # 设置Y2轴标题
+    ax2.grid(False)
+    ax2.xaxis.set_major_locator(ticker.MultipleLocator(12))
+
+    ax3 = ax1.twinx()
+    ax3.spines["right"].set_position(("axes", 1.2))
+    make_patch_spines_invisible(ax3)
+    ax3.spines["right"].set_visible(True)
+    p3, = ax3.plot(x, y4, color=color_y4, ms=10, label=label_y4)
+    ax3.set_ylabel(label_y4)
+    ax3.grid(False)
+    ax3.xaxis.set_major_locator(ticker.MultipleLocator(12))
+    ax3.yaxis.label.set_color(p3.get_color())
 
     # 添加标签
     ax1.legend(loc='upper left')
     ax2.legend(loc='upper right')
+    ax3.legend(loc='lower right')
+
     plt.title(title)  # 添加标题
     plt.grid(axis="y")  # 背景网格
-
-    # 画A股,参考：https://deepinout.com/matplotlib/matplotlib-axis/matplotlib-the-hidden-scale-mode-shows-the-three-y-axes.html
-    # 创建第三条Y轴，把第三条Y轴的其它三边隐藏起来，只留下右边显示
-    def make_patch_spines_invisible(ax):
-        ax.set_frame_on(True)
-        ax.patch.set_visible(False)
-        for sp in ax.spines.values():
-            sp.set_visible(False)
-
-    par2 = ax1.twinx()
-    par2.spines["right"].set_position(("axes", 1.2))
-    make_patch_spines_invisible(par2)
-    par2.spines["right"].set_visible(True)
-    p3, = par2.plot(x, y4, "g-", label="上证指数")
-    par2.set_ylabel("上证指数")
-    par2.legend(loc='upper right')
-    par2.yaxis.label.set_color(p3.get_color())
 
     # 保存图片
     factor = '' if len(factor_names) > 1 else factor_names[0]
